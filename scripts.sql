53c064e7c7ade3a0c85b556a2b6614a4ee352317
ursu96 <40837351+ursu96@users.noreply.github.com>
2019-02-25 19:07:37 +0200

    Update scripts.sql

--- scripts.sql
+++ scripts.sql
@@ -1,7 +1,7 @@
 CREATE TABLE [dbo].[CLIENTI] (
     [IdClient] INT            NOT NULL,
     [name]     NVARCHAR (MAX) NULL,
-    [suma]     FLOAT (53)     NULL,
+    [suma]     FLOAT (54)     NULL,
     [rating]   INT            NULL,
     PRIMARY KEY CLUSTERED ([IdClient] ASC)
 );
